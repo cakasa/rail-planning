4f2dfe7846c2878a9ef74e58e577549508dfa257
Tim Tian <t.tian-2@student.tudelft.nl>
2023-05-13 15:40:43 +0200

    Adds msp2: simpler version of msp1 with less tracks, same amount of trains

--- ProblemGenerationFile.py
+++ ProblemGenerationFile.py
@@ -86,7 +86,7 @@ def create_problem_file(filename, name, arrival_order, departure_train_units, tr
         f.write(")))\n")
 
 if __name__ == "__main__":
-    create_problem_file("D:\\DelftFiles\\3Q4\\GitHub\\rail-planning\\multipleStartProblem1.pddl", "multipleStartProblem1", 
+    create_problem_file("D:\\DelftFiles\\3Q4\\GitHub\\rail-planning\\multipleStartProblem2.pddl", "multipleStartProblem2", 
                         ["sng", "icm", "slt", "virm", "virm", "sng", "icm", "slt", "sng", "icm"], 
                         ["sng", "icm", "slt", "sng", "icm", "sng", "icm", "slt", "virm", "virm"], 
                         [(0,1), (1,2), (2,3), (3,4), (0,5), (5,6), (6,7), (7,8), (0,9), (9,10), (10,11), (11,12), (0,13), (13,14), (14,15), (15,16)], 
