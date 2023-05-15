7fabbb280f20fc23a68d9017690418a385984ea1
Bozhidar Andonov <cakas@abv.bg>
2023-05-15 20:59:36 +0200

    Add problem 4

---
+++ servicing-no-fluents/generators/problem04.py
@@ -0,0 +1,46 @@
+import os
+from problem_generator import create_problem_file
+
+problem_name = os.path.basename(__file__)[:-3]
+filename = '../' + problem_name + '.pddl'
+trains = {
+    'train1': ('slt', 4, ['cleaning']),
+    'train2': ('sng', 2, ['inspection']),
+    'train3': ('slt', 5, []),
+    'train4': ('slt', 3, []),
+    'train5': ('sng', 3, ['cleaning'])
+}
+arrival_order = ['train1', 'train2', 'train3', 'train4', 'train5']
+departure_order = ['train2', 'train5', 'train1', 'train3', 'train4']
+tracks = {
+   'track0': (5, []),
+   'track1': (5, ['cleaning']),
+   'track2': (2, []),
+   'track3': (5, ['inspection']),
+   'track4': (2, []),
+   'track5': (5, []),
+   'track6': (5, []),
+   'track7': (5, []),
+   'track8': (5, []),
+}
+next_to = [
+    ('track0', 'track1'), 
+    ('track0', 'track2'),
+    ('track2', 'track3'),
+    ('track2', 'track4'),
+    ('track4', 'track5'),
+    ('track4', 'track6'),
+    ('track4', 'track7'),
+    ('track4', 'track8'),
+]
+
+create_problem_file(filename, problem_name, trains, arrival_order, departure_order, tracks, next_to)
+''' Creating PDDL problem files
+    - `filename` for creating the file
+    - `problem_name` is the name to be given to the problem instance
+    - `trains` is a dictionary, key = name, value = a 3-tuple of the form (train_type, train_length, needed_services)
+    - `arrival_order` is a list of the order of the arriving trains (the train with index 0 arrives first)
+    - `departure_order` is a list of the order of the departing trains (the train with index 0 departs last)
+    - `tracks` is a dictionary, key = trackname, value = (length, services)`
+    - `next_to` - a list of adjacent track pairs (where first track's last element is adjacent to the second track's first element)
+'''
