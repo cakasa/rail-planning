5bc5856c3a8dfe35709dd9d00c62e5477b2e6176
Bozhidar Andonov <cakas@abv.bg>
2023-05-17 14:03:27 +0200

    Update problem 4

--- servicing-no-fluents/generators/problem04.py
+++ servicing-no-fluents/generators/problem04.py
@@ -8,10 +8,10 @@ trains = {
     'train2': ('sng', 2, ['inspection']),
     'train3': ('sng', 1, []),
     'train4': ('slt', 2, []),
-    # 'train5': ('sng', 3, []),
+    'train5': ('sng', 3, []),
 }
-arrival_order = ['train1', 'train2', 'train3', 'train4']
-departure_order = ['train2', 'train1', 'train3', 'train4']
+arrival_order = ['train1', 'train2', 'train3', 'train4', 'train5']
+departure_order = ['train2', 'train1', 'train3', 'train4', 'train5']
 tracks = {
    'track0': (4, []),
    'track1': (4, ['cleaning']),
