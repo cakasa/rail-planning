bc7353d09cc8f6c737d5e0972a7fe3ec770d1c14
Bozhidar Andonov <cakas@abv.bg>
2023-05-21 00:03:16 +0200

    Update evaluation script

--- servicing-simple/eval/eval.sh
+++ servicing-simple/eval/eval.sh
@@ -2,7 +2,7 @@
 mkdir $1
 cd $1
 
-problems="small_two_trains small_five_trains medium_five_trains big_ten_trains medium_ten_trains big_twenty_trains small_eight_trains medium_fifteen_trains medium_twenty_trains big_thirty_trains"
+problems="small_two_trains small_five_trains medium_five_trains big_ten_trains medium_ten_trains big_twenty_trains small_eight_trains medium_fifteen_trains big_thirty_trains medium_twenty_trains"
 
 for problem in $problems
 do
@@ -12,7 +12,8 @@ do
     do
         mkdir run$run
         cd run$run
-        { time /data/ipc2018/solvers/sat/$1/planner.img ./../../../../servicing-simple.pddl ./../../../../$problem.pddl ./re.out ; } |& tee console.txt
+        { time /data/ipc2018/solvers/sat/$1/planner.img ./../../../../servicing-simple.pddl ./../../../../$problem.pddl ./re.out ; } &> console.txt
+        echo "Planner $1: Done evaluating $problem run $run"
         cd ..
     done
     cd ..
