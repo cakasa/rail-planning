e1d26b4631b81bc07e789f4146897d91abe26f15
Bozhidar Andonov <cakas@abv.bg>
2023-05-21 16:22:03 +0200

    Update eval_further.sh

--- servicing-simple/eval/eval_further.sh
+++ servicing-simple/eval/eval_further.sh
@@ -6,12 +6,12 @@ problems="big_twenty_trains small_eight_trains medium_fifteen_trains big_thirty_
 
 for problem in $problems
 do
-    mkdir extra$problem
-    cd extra$problem
+    mkdir "extra$problem"
+    cd "extra$problem"
     mkdir run1
     cd run1
     { time /data/ipc2018/solvers/sat/$1/planner.img ./../../../../servicing-simple.pddl ./../../../../$problem.pddl ./re.out ; } &> console.txt
     echo "Planner $1: Done evaluating $problem run1"
     cd ..
-    cd ..
+    cd .
 done
