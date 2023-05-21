a9c2108043ed497cccc84d8826b7ff12b7c8c58d
Bozhidar Andonov <cakas@abv.bg>
2023-05-21 16:22:53 +0200

    Fix bug in eval_further

--- servicing-simple/eval/eval_further.sh
+++ servicing-simple/eval/eval_further.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-mkdir $1
-cd $1
+mkdir extra$1
+cd extra$1
 
 problems="big_twenty_trains small_eight_trains medium_fifteen_trains big_thirty_trains"
 
 for problem in $problems
 do
-    mkdir "extra$problem"
-    cd "extra$problem"
+    mkdir $problem
+    cd $problem
     mkdir run1
     cd run1
     { time /data/ipc2018/solvers/sat/$1/planner.img ./../../../../servicing-simple.pddl ./../../../../$problem.pddl ./re.out ; } &> console.txt
