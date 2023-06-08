c6a474d5277dbf5c8607623db00ed16444a89b3c
Ákos Soma Sárkány <sarkany@student.tudelft.nl>
2023-06-08 14:14:05 +0200

    Added some output for feedback.

--- experiments/runner-v2.py
+++ experiments/runner-v2.py
@@ -3,7 +3,9 @@ import subprocess as sp
 
 def execute(exe: str, cwd: str, problem: str, settings: int, timeout: int):
     domain = "domain5.pddl"
+    print(f"Start search with option [{settings}] on file '{problem}' at {datetime.datetime.now()}")
     result = process(exe, cwd, domain, problem, settings, timeout)
+    print(f"Search finished. Writing to file.")
 
     if not os.path.isdir(os.path.join(cwd, "results")):
         os.mkdir(os.path.join(cwd, "results"))
@@ -17,7 +19,6 @@ def execute(exe: str, cwd: str, problem: str, settings: int, timeout: int):
         f.close()
 
 def process(exe: str, path: str, domain: str, problem: str, settings: int, to: int) -> str:
-    id = os.getpid()
     cmd = f"{exe} -p {path}/ -o {domain} -f problems/{problem} -s {settings}"
     try:
         sub = sp.check_output(cmd.split(), timeout=to)
@@ -29,23 +30,21 @@ def process(exe: str, path: str, domain: str, problem: str, settings: int, to: i
 if __name__ == "__main__":
     exe = "/data/Metric-FF-v2.1/ff"
     timeout = 3600
-    if len(sys.argv) >= 2:
-        try:
-            timeout = int(sys.argv[1])
-        except:
-            print("Optional positional for timeout not specified. Continuing with default value = 3600s")
+    try:
+        timeout = int(sys.argv[1])
+    except:
+        print("Optional positional for timeout not specified. Continuing with default value = 3600s")
 
-    if len(sys.argv) >= 3:
-        try:
-            if int(sys.argv[2]) == 1:
-                exe = "ff-v2.1.exe"
-        except:
-            print("Optional positional argument must be 1 or 0!\n\tDefault: 0 for mapfw server\n\tUse 1 on PC")
-            exit(1)
+    try:
+        if int(sys.argv[2]) == 1:
+            exe = "ff-v2.1.exe"
+    except:
+        print("Optional positional argument for sysytem not specified.\n\tDefault: 0 for mapfw server\n\tUse 1 on PC")
 
 
     cwd = os.getcwd()
     ps = os.listdir("problems")
+    print(f"Executing ff for the following files: {ps}")
 
     for p in ps:
         for i in range(6):
