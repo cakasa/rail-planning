0fc500f12520f1671ac186f0e1ccfc9e37501336
Bozhidar Andonov <cakas@abv.bg>
2023-06-18 17:02:29 +0200

    Add script for evaluation of improved planner

---
+++ servicing-simple/eval/eval_improved.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir improved
+cd improved
+
+problems="small_two_trains small_five_trains medium_five_trains big_ten_trains medium_ten_trains big_twenty_trains small_eight_trains medium_fifteen_trains big_thirty_train"
+
+for problem in $problems
+do
+    mkdir $problem
+    cd $problem
+    for run in {1..5}
+    do
+        mkdir run$run
+        cd run$run
+        { time ./../../../../team4/p.img ./../../../../servicing-simple.pddl ./../../../../$problem.pddl ./re.out ; } &> console.txt
+        echo "Done evaluating$ problem run $run"
+        cd ..
+    done
+    cd ..
+done
