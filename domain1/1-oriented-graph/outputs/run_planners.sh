c5f3a92f9d8cb72f2a1eb4beb2e776e181f521be
Simba <simba.c12@gmail.com>
2023-06-18 13:20:53 +0200

    Modify script

--- domain1/1-oriented-graph/outputs/run_planners.sh
+++ domain1/1-oriented-graph/outputs/run_planners.sh
@@ -1,29 +1,8 @@
-PLANNER=explicit-baseline-planners
-mkdir $PLANNER
-cd $PLANNER
-/data/ipc2018/solvers/sat/$PLANNER/planner.img ../../domain1.pddl ../../problem2.pddl re.out > $PLANNER.log
-cd ../
-echo $PLANNER DONE
-
-PLANNER=team2
-mkdir $PLANNER
-cd $PLANNER
-/data/ipc2018/solvers/sat/$PLANNER/planner.img ../../domain1.pddl ../../problem2.pddl re.out > $PLANNER.log
-cd ../
-echo $PLANNER DONE
-
-PLANNER=team4
-mkdir $PLANNER
-cd $PLANNER
-/data/ipc2018/solvers/sat/$PLANNER/planner.img ../../domain1.pddl ../../problem2.pddl re.out > $PLANNER.log
-cd ../
-echo $PLANNER DONE
-
-PLANNER=team7
-mkdir $PLANNER
-cd $PLANNER
-/data/ipc2018/solvers/sat/$PLANNER/planner.img ../../domain1.pddl ../../problem2.pddl re.out > $PLANNER.log
-cd ../
-echo $PLANNER DONE
-
-unset PLANNER
+for PLANNER in explicit-baseline-planner team2 team4 team7
+do
+    mkdir $PLANNER
+    cd $PLANNER
+    /data/ipc2018/solvers/sat/$PLANNER/planner.img ../../domain1.pddl ../../problem2.pddl re.out > $PLANNER.log
+    cd ../
+    echo $PLANNER DONE
+done
