19177a6467c8cfe623bc57e5722062f729243d7a
Ákos Soma Sárkány <sarkany@student.tudelft.nl>
2023-06-20 22:05:55 +0200

    corrected wrong executeable

--- script1.sh
+++ script1.sh
@@ -1,4 +1,5 @@
 mkdir results
+EXE=./data/Metric-FF-v2.1/ff
 
 for SETTING in 2 0 1
 do
@@ -8,7 +9,7 @@ do
         THEN=$(date +"%Y-%m-%d %T.%N" | cut -b 1-23)
         echo "Straring $PROBLEM with setting $SETTING at $THEN" 
         echo "Straring $PROBLEM with setting $SETTING at $THEN" > results/s$SETTING/result-$PROBLEM-setting-$SETTING.out
-        timeout 30m ./ff-v2.1.exe -p ./ -o tusd.pddl -f problems/$PROBLEM.pddl -w 10 -s $SETTING >> results/s$SETTING/result-$PROBLEM-setting-$SETTING.out
+        timeout 30m $EXE -p ./ -o tusd.pddl -f problems/$PROBLEM.pddl -w 10 -s $SETTING >> results/s$SETTING/result-$PROBLEM-setting-$SETTING.out
         NOW=$(date +"%Y-%m-%d %T.%N" | cut -b 1-23)
         echo "Finished $PROBLEM with setting $SETTING at $NOW"
         SD=$(date -u -d "$THEN" +"%s.%N")
