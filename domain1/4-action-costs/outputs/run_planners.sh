0486592cbbdb9874fb19fc07ec8602df64b25a41
Simba <simba.c12@gmail.com>
2023-06-20 18:24:54 +0200

    Run all planners

--- domain1/4-action-costs/outputs/run_planners.sh
+++ domain1/4-action-costs/outputs/run_planners.sh
@@ -1,4 +1,4 @@
-for PLANNER in explicit-baseline-planners team2 team4 team7
+for PLANNER in explicit-baseline-planners team1 team2 team3 team4 team6 team7 team8 team10 team14 team15 team16 team18  team20 team30 team31  team33 team34 team35 team36  team43 team45 team47
 do
     mkdir $PLANNER
     cd $PLANNER
